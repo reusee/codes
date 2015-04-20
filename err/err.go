712b18db1be595278a4496eba31f099b74c34c79
reus <reusee@gmail.com>
2015-04-20 17:03:18 +0800

    add err package

---
+++ err/err.go
@@ -0,0 +1,23 @@
+package err
+
+import "fmt"
+
+var Pkg = "packageName"
+
+type Err struct {
+	Pkg  string
+	Info string
+	Err  error
+}
+
+func (e *Err) Error() string {
+	return fmt.Sprintf("%s: %s\n%v", e.Pkg, e.Info, e.Err)
+}
+
+func makeErr(err error, info string) *Err {
+	return &Err{
+		Pkg:  Pkg,
+		Info: info,
+		Err:  err,
+	}
+}
